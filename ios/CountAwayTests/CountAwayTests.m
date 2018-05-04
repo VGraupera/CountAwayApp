3ab11a875f3fe15f64e997d6adc478012b571a75
Vidal Graupera <vgraupera@gmail.com>
2018-05-03 22:25:16 -0700

    chore(various): rename ios and android projects from countawayapp -> countaway

--- ios/CountAwayTests/CountAwayTests.m
+++ ios/CountAwayTests/CountAwayTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface CountAwayAppTests : XCTestCase
+@interface CountAwayTests : XCTestCase
 
 @end
 
-@implementation CountAwayAppTests
+@implementation CountAwayTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
