af01ffb624d31590591d59f37ae3c8e90a31033f
Vidal Graupera <vgraupera@gmail.com>
2018-05-03 22:09:15 -0700

    feature(google-analytics): initial add of google analytics

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import DaysOff from './components/DaysOff';
 import Event from './components/Event';
+import { tracker } from './utility/analytics';
 
 const Navigator = StackNavigator({
   Event: { screen: Event },
   DaysOff: { screen: DaysOff },
 });
+
+// gets the current screen from navigation state
+function getCurrentRouteName(navigationState) {
+  if (!navigationState) {
+    return null;
+  }
+  const route = navigationState.routes[navigationState.index];
+  // dive into nested navigators
+  if (route.routes) {
+    return getCurrentRouteName(route);
+  }
+  return route.routeName;
+}
 
 export default class App extends React.Component {
   constructor() {
@@ -14,6 +28,15 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <Navigator />;
+    return <Navigator
+      onNavigationStateChange={(prevState, currentState) => {
+        const currentScreen = getCurrentRouteName(currentState);
+        const prevScreen = getCurrentRouteName(prevState);
+
+        if (prevScreen !== currentScreen) {
+          tracker.trackScreenView(currentScreen);
+        }
+      }}
+    />;
   }
 }
---
+++ src/utility/analytics.js
@@ -0,0 +1,5 @@
+import {
+  GoogleAnalyticsTracker,
+} from 'react-native-google-analytics-bridge';
+
+export const tracker = new GoogleAnalyticsTracker('UA-2153940-16');
